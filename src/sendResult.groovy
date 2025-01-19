e9339f36f77f30698674cf8f4fcb7a22dfb0df3f
Damon Edstrom <dedstrom@venerated.io>
2025-01-19 03:44:50 -0600

    Initial commit; email generation testing

---
+++ src/sendResult.groovy
@@ -0,0 +1,17 @@
+def call(Map config) {
+    echo 'Sending notification...'
+    def rawBody = libraryResource 'io/venerated/templates/build-results.html'
+    def binding = [
+            applicationName: env.JOB_BASE_NAME,
+            sourceBranch   : env.GIT_BRANCH,
+            buildNumber    : env.BUILD_NUMBER,
+            userName       : currentBuild.getBuildCauses()[0].userId,
+            buildUrl       : env.BUILD_URL
+    ]
+
+    def render = renderTemplate(rawBody, binding)
+    // TODO: Stdout for testing, swap to email plugin
+    echo render
+    def subjectLine = "${env.JOB_BASE_NAME} - ${env.BUILD_NUMBER} - ${currentBuild.currentResult}"
+    echo subjectLine
+}
