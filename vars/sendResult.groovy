e93e1a04d1910d32ff582f92be4da1b3bc5b1355
Damon Edstrom <dedstrom@venerated.io>
2025-01-28 17:10:32 -0600

    Added default ternary to binding vars as template engine doesn't appear to support existence checks without workarounds

--- vars/sendResult.groovy
+++ vars/sendResult.groovy
@@ -2,18 +2,19 @@ def call(Map config) {
     echo 'Sending notification...'
     def rawBody = libraryResource 'io/venerated/templates/build-results.html'
     def binding = [
-            applicationName: env.JOB_BASE_NAME,
-            sourceBranch   : env.GIT_BRANCH,
-            buildNumber    : env.BUILD_NUMBER,
-            userName       : currentBuild.getBuildCauses()[0].userId,
-            buildUrl       : env.BUILD_URL,
-            mavenCmd       : params.maven_command,
-            testVar        : config.someVar
+            applicationName: env.JOB_BASE_NAME ?: "Not Found",
+            sourceBranch   : env.GIT_BRANCH ?: "Not Found",
+            buildNumber    : env.BUILD_NUMBER ?: "Not Found",
+            userName       : currentBuild.getBuildCauses()[0].userId ?: "Not Found",
+            buildUrl       : env.BUILD_URL ?: "Not Found",
+            mavenCmd       : params.maven_command ?: "Not Found",
+            testVar        : config.someVar ?: "Not Found",
+            condVar        : config.condVar ?: null
     ]
 
-    if (config.condVar) {
-        binding.condVar = config.condVar
-    }
+//    if (config.condVar) {
+//        binding.condVar = config.condVar
+//    }
 
     def render = renderTemplate(rawBody, binding)
     // TODO: Stdout for testing, swap to email plugin
