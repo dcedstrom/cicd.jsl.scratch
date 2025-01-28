4600a0b80d9447a52bbdbb04ae946ec03d6d1bc0
Damon Edstrom <dedstrom@venerated.io>
2025-01-28 15:48:01 -0600

    Added param input (`config` map) for inline vars; added emailExt callout to actually send email

--- vars/sendResult.groovy
+++ vars/sendResult.groovy
@@ -7,13 +7,22 @@ def call(Map config) {
             buildNumber    : env.BUILD_NUMBER,
             userName       : currentBuild.getBuildCauses()[0].userId,
             buildUrl       : env.BUILD_URL,
-            mavenCmd: params.maven_command,
-            testVar: someVar
+            mavenCmd       : params.maven_command,
+            testVar        : config.someVar
     ]
+
+    if (config.condVar) {
+        binding.condVar = config.condVar
+    }
 
     def render = renderTemplate(rawBody, binding)
     // TODO: Stdout for testing, swap to email plugin
     echo render
     def subjectLine = "${env.JOB_BASE_NAME} - ${env.BUILD_NUMBER} - ${currentBuild.currentResult}"
     echo subjectLine
+
+    emailExt body: render,
+        subject: subjectLine,
+//        to: params.emailRecipients
+        to: 'dedstrom@venerated.io'
 }
