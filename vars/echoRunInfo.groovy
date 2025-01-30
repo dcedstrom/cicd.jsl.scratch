1810e7ce923470787fa7253a4338fb91364331d7
Damon Edstrom <dedstrom@venerated.io>
2025-01-29 18:04:44 -0600

    Fixed map output; added formatting to env output

--- vars/echoRunInfo.groovy
+++ vars/echoRunInfo.groovy
@@ -1,8 +1,10 @@
+import groovy.json.JsonOutput
+
 def call(Map config) {
     echo 'Showing input params'
-    echo config
+    echo JsonOutput.prettyPrint(JsonOutput.toJson(config))
 
     echo 'Showing available env vars'
-    sh "env"
+    sh "env | sort | jq -Rn '[inputs | split(\"=\") | {key:.[0], value:.[1]}] | from_entries'"
 
 }
