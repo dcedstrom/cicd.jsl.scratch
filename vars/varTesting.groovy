f7f2d71daeb242083fd89c827028d6df75018b20
Damon Edstrom <dedstrom@venerated.io>
2025-01-29 19:21:36 -0600

    Added var testing function

---
+++ vars/varTesting.groovy
@@ -0,0 +1,42 @@
+def Call(Map config) {
+    echo "Param var..."
+    try {
+        echo "${params.param_var}"
+    } catch (e) {
+        echo "Param var did not work"
+        echo "Exception ${e}"
+    }
+
+    echo "`config` var..."
+    try {
+        echo "${config.config_var}"
+    } catch (e) {
+        echo "COnfig var did not work"
+        echo "Exception ${e}"
+    }
+
+    echo "Env Var..."
+    try {
+        echo "Build Number: ${env.BUILD_NUMBER}"
+    } catch (e) {
+        echo "Env var did not work"
+        echo "Exception ${e}"
+    }
+
+    echo "Stage var..."
+    try {
+        echo "${pipeline_stage_var}"
+    } catch (e) {
+        echo "Pipeline stage var did not work"
+        echo "Exception ${e}"
+    }
+
+
+    echo "Global var..."
+    try {
+        echo "${pipeline_global_var}"
+    } catch (e) {
+        echo "Pipeline global var did not work"
+        echo "Exception ${e}"
+    }
+}
