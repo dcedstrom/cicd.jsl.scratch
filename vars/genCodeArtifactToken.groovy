c69ca93b6fc40ba680f766a8fb8148648051236f
Damon Edstrom <dedstrom@venerated.io>
2025-10-29 15:56:51 -0500

    Change role to credentials since external from AWS

--- vars/genCodeArtifactToken.groovy
+++ vars/genCodeArtifactToken.groovy
@@ -1,5 +1,5 @@
 def call(Map config) {
-    withAWS(role: config.iam_role, region: config.aws_region) {
+    withAWS(credentials: config.AWSCredentialName, region: config.aws_region) {
         def codeArtifactToken = sh(returnStdout: true, script: """
         aws codeartifact get-authorization-token --region ${config.aws_region} --domain ${config.codeArtifactDomain} --domain-owner ${config.codeArtifactDomainOwner} --query authorizationToken --output text
     """).trim()
