4c558628b27f4c815e974981ec680b4ce1b61ecd
Damon Edstrom <dedstrom@venerated.io>
2025-11-04 16:45:10 -0600

    Fixed ecr login sh call to have kwarg

--- vars/ecrLogin.groovy
+++ vars/ecrLogin.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
     withAWS(region: config.region, credentials: config.credentials) {
-        dockerLogin = sh("aws ecr get-login-password --region ${config.region} | docker login --username AWS --password-stdin ${config.repo}", returnStdout: true)
+        dockerLogin = sh(script: "aws ecr get-login-password --region ${config.region} | docker login --username AWS --password-stdin ${config.repo}", returnStdout: true)
     }
 
     return dockerLogin
